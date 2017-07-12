f9fa4f94287dd45df36273299953b56714bcde6e
Doug <doug.steinberg@gmail.com>
2017-07-12 16:01:32 -0400

    Exercise complete

--- cats.js
+++ cats.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var summonKitties = document.querySelector('.summon-cats');
-  console.log(summonKitties)
+  var catNumber = 1;
 
   summonKitties.addEventListener('click', function(event) {
     console.log('CLICK')
@@ -14,15 +14,9 @@ document.addEventListener("DOMContentLoaded", function() {
         var catPhoto = document.createElement('img');
         var catPhotoSrc = responseData.cats[0].photo;
         catPhoto.src = catPhotoSrc;
-        document.querySelector('.cat-box').append(catPhoto);
+        document.querySelector('#cat'+catNumber).append(catPhoto);
+        catNumber++;
       });
     });
 
 });
-
-// document.querySelector('#step3456').append(response);
-
-// Creates a new <img> tag
-// Sets the src attribute of the <img> to the cat's photo
-// Sets the alt attribute of the <img> to "Photo of (insert cat name here)"
-// Inserts that <img> into one of the empty .cat-box divs
